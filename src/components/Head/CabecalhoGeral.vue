b87dc3bc8975bbb0d38150a63efff508dae2b7c5
Felipe Tavares <felipelimatavares96@gmail.com>
2022-09-24 19:23:58 -0300

    primeiro commit

---
+++ src/components/Head/CabecalhoGeral.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <nav> HELLO WORLD</nav>
+    <img src="https://i.picsum.photos/id/1/5616/3744.jpg?hmac=kKHwwU8s46oNettHKwJ24qOlIAsWN9d2TtsXDoCWWsQ" alt="Imagem Header">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'CabecalhoGeral'
+}
+</script>
+
+<style>
+  header{
+    border: 1px solid red;
+    width: 100%;
+    height: 100vh;
+    background-color: black;
+    overflow: hidden;
+  }
+  header>nav{
+    width: 100%;
+    height: 80px;
+    background-color: rgb(255, 228, 196, 0.1);
+    position: absolute;
+  }
+  header>img{
+    object-fit: cover;
+    width: 100%;
+    height: 100vh;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <CabecalhoGeral></CabecalhoGeral>
+  </div>
+</template>
+
+<script>
+import CabecalhoGeral from '@/components/Head/CabecalhoGeral.vue';
+
+export default {
+    components: { CabecalhoGeral }
+}
+</script>
+
+<style>
+
+</style>
