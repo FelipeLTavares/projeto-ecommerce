29e6f45c8d3737b9319f67f6d849fa965b000ac4
Felipe Tavares <felipelimatavares96@gmail.com>
2022-09-30 10:41:29 -0300

    Adicao do menu suspenso

--- src/components/Head/CabecalhoGeral.vue
+++ src/components/Head/CabecalhoGeral.vue
@@ -1,23 +1,33 @@
 <template>
-  <div class="capa">
-    <header>
-      <h5>LOGOTIPO</h5>
-      <div class="menu">
-        <span class="itemMenu">INICIO</span>
-        <span class="itemMenu">PRODUTOS</span>
-        <span class="itemMenu">CONTATO</span>
-        <MenuIcon class="menuIc"></MenuIcon>
+  <div class="geral">
+    <div class="capa">
+      <header>
+        <h5>LOGOTIPO</h5>
+        <div class="menu">
+          <span class="itemMenu">INICIO</span>
+          <span class="itemMenu">PRODUTOS</span>
+          <span class="itemMenu">CONTATO</span>
+          <MenuIcon class="menuIc" @click="mostrarMenu()"></MenuIcon>
+        </div>
+      </header>
+        <div class="titulo">
+          <span>TITULO</span>
+        </div>
+        <div class="texto">
+          <span>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          </span>
+        </div>
       </div>
-    </header>
-      <div class="titulo">
-        <span>TITULO</span>
+      <div class="menuSus" v-show="mostrar">
+        <div class="menuSus2">
+          <span class="itemMenu">INICIO</span>
+          <span class="itemMenu">PRODUTOS</span>
+          <span class="itemMenu">CONTATO</span>
+        </div>
+        <div class="menuSus3" @click="mostrarMenu()"></div>
       </div>
-      <div class="texto">
-        <span>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-        </span>
-      </div>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -27,11 +37,55 @@ export default {
   name: 'CabecalhoGeral',
   components:{
     MenuIcon
+  },
+  data(){
+    return{
+      mostrar: true
+    }
+  },
+  methods: {
+    mostrarMenu(){
+      this.mostrar = !this.mostrar
+    }
   }
 }
 </script>
 
 <style>
+  .menuSus3{
+    width: 60%;
+    height: 100vh;
+    background-color: transparent;
+  }
+  .menuSus2{
+    width: 40%;
+    height: 100vh;
+    background-color: #202020;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 40px;
+  }
+  .menuSus2>span{
+    width: 100%;
+    text-align: center;
+    padding: 20px 0;
+  }
+  .menuSus2>span:hover{
+    background-color: gray;
+  }
+  .menuSus{
+    width: 100%;
+    height: 100vh;
+    background-color: rgb(0, 0, 0, 0.6);
+    z-index: 13;
+    top: 0;
+    position: absolute;
+    display: flex;
+    flex-direction: row-reverse;
+  }
+
   .capa{
     width: 100%;
     height: 60vh;
@@ -64,7 +118,6 @@ export default {
     font-weight: 700;
     font-size: 24px;
   }
-
   .menuIc{
     display: none;
   }
@@ -114,6 +167,15 @@ export default {
     }
     .texto{
       display:none;
+    }
+  }
+
+  @media(max-width: 500px){
+    .menuSus3{
+      width: 40%
+    }
+    .menuSus2{
+      width: 60%;
     }
   }
 </style>
